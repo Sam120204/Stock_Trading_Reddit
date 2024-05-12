e33291f93ba895090290e8df4ddc703bc3c1230c
Sam120204 <zhongjiayou1202@gmail.com>
2024-05-12 14:37:50 -0400

    Update initial files

---
+++ config.py
@@ -0,0 +1,6 @@
+# Configuration for Reddit API
+REDDIT_CLIENT_ID = 'your_client_id'
+REDDIT_SECRET = 'your_client_secret'
+REDDIT_USER_AGENT = 'your_user_agent'
+REDDIT_USERNAME = 'your_username'
+REDDIT_PASSWORD = 'your_password'
