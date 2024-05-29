d9a53be2893ace42d97570e74b19aa4bb2edc4f5
Sam120204 <zhongjiayou1202@gmail.com>
2024-05-29 16:20:18 -0400

    updtating the training model using random forest regressor

--- testing/src/model_training/training.py
+++ testing/src/model_training/training.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import joblib
 
@@ -21,7 +21,8 @@ def train_model(data):
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    model = LinearRegression()
+    # Use RandomForestRegressor instead of LinearRegression
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     
     return model, X_test, y_test
