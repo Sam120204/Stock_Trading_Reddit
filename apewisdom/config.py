47b5a52f22b938a0fd54fef1d598a6b51cdfaaec
Sam120204 <zhongjiayou1202@gmail.com>
2024-06-14 14:46:53 -0400

    Updating config.py in integrate with streamlit

--- apewisdom/config.py
+++ apewisdom/config.py
@@ -1,21 +1,37 @@
 from urllib.parse import quote_plus
-from dotenv import load_dotenv
+import streamlit as st
 import os
+from dotenv import load_dotenv
 
-load_dotenv()
+# Load environment variables from .env if not running in Streamlit
+if not st.secrets:
+    load_dotenv()
 
-REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
-REDDIT_SECRET = os.getenv('REDDIT_SECRET')
-REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
-REDDIT_USERNAME = os.getenv('REDDIT_USERNAME')
-REDDIT_PASSWORD = os.getenv('REDDIT_PASSWORD')
+# Function to get secret or environment variable
+def get_config_value(secret_section, secret_key, env_key):
+    return st.secrets.get(secret_section, {}).get(secret_key, os.getenv(env_key))
 
-username = quote_plus(os.getenv('MONGO_USERNAME'))
-password = quote_plus(os.getenv('MONGO_PASSWORD'))
-MONGO_URI = f"mongodb+srv://{username}:{password}@trendsage.svfhdvw.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
+# Reddit configuration
+REDDIT_CLIENT_ID = get_config_value("reddit", "client_id", 'REDDIT_CLIENT_ID')
+REDDIT_SECRET = get_config_value("reddit", "client_secret", 'REDDIT_SECRET')
+REDDIT_USER_AGENT = get_config_value("reddit", "user_agent", 'REDDIT_USER_AGENT')
+REDDIT_USERNAME = get_config_value("reddit", "username", 'REDDIT_USERNAME')
+REDDIT_PASSWORD = get_config_value("reddit", "password", 'REDDIT_PASSWORD')
 
-DB_NAME = os.getenv('DB_NAME')
-COLLECTION_NAME = os.getenv('COLLECTION_NAME')
+# MongoDB configuration
+mongo_username = get_config_value("mongo", "username", 'MONGO_USERNAME')
+mongo_password = get_config_value("mongo", "password", 'MONGO_PASSWORD')
 
-NEWSAPI_KEY = os.getenv('NEWSAPI_KEY')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+if mongo_username is None or mongo_password is None:
+    raise ValueError("MongoDB username or password is not set.")
+
+username = quote_plus(mongo_username)
+password = quote_plus(mongo_password)
+MONGO_URI = get_config_value("mongo", "uri", f"mongodb+srv://{username}:{password}@trendsage.svfhdvw.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true")
+
+DB_NAME = get_config_value("mongo", "db_name", 'DB_NAME')
+COLLECTION_NAME = get_config_value("mongo", "collection_name", 'COLLECTION_NAME')
+
+# API keys
+NEWSAPI_KEY = get_config_value("newsapi", "key", 'NEWSAPI_KEY')
+OPENAI_API_KEY = get_config_value("openai", "api_key", 'OPENAI_API_KEY')
