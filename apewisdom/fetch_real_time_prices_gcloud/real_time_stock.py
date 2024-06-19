c19b48fd89f8b125879834d58612234e0ac6c012
bobzheng <shenyanzheng00@gmail.com>
2024-06-18 23:40:10 -0400

    build real time stock price fetching and storing and deploy to Google Cloud Function for automation

---
+++ apewisdom/fetch_real_time_prices_gcloud/real_time_stock.py
@@ -0,0 +1,55 @@
+import yfinance as yf
+import pandas as pd
+from pymongo import MongoClient
+import config
+import logging
+
+def get_real_time_stock_price(ticker):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period='1d')
+    if not data.empty:
+        price_data = {
+            'ticker': ticker,
+            'price': data['Close'][0],
+            'volume': data['Volume'][0],
+            'open': data['Open'][0],
+            'high': data['High'][0],
+            'low': data['Low'][0],
+            'close': data['Close'][0],
+            'date': data.index[0]
+        }
+        return price_data
+    else:
+        return None
+
+
+def fetch_tickers_from_mongo():
+    client = MongoClient(config.MONGO_URI)
+    db = client[config.DB_NAME]
+    collection = db[config.COLLECTION_NAME]
+    tickers = collection.distinct('ticker')
+    return tickers
+
+
+def update_real_time_prices():
+    tickers = fetch_tickers_from_mongo()
+    stock_prices = []
+    for ticker in tickers:
+        try:
+            price_data = get_real_time_stock_price(ticker)
+            if price_data:
+                stock_prices.append(price_data)
+        except Exception as e:
+            logging.info(f"Error fetching data for {ticker}: {e}")
+
+    logging.info(f"Real-time stock prices fetched successfully.")
+    return pd.DataFrame(stock_prices)
+
+
+def save_real_time_prices_to_mongo(prices_df):
+    client = MongoClient(config.MONGO_URI)
+    db = client[config.DB_NAME]
+    collection = db['real_time_prices']
+    records = prices_df.to_dict('records')
+    collection.insert_many(records)
+    logging.info(f"Real-time prices saved to MongoDB successfully.")
