52ffdbce07524fcf52e811dcc365cf7e72cf2cab
bobzheng <shenyanzheng00@gmail.com>
2024-06-25 10:25:44 -0400

    updating stock prices fetching so database credential is hidden

--- stock/fetch_store_stock_prices.py
+++ stock/fetch_store_stock_prices.py
@@ -1,22 +1,40 @@
 import psycopg2
 import yfinance as yf
-from datetime import datetime
+import dotenv
+import os
+from datetime import datetime, timedelta
 
-# PostgreSQL connection details
+dotenv.load_dotenv()
+
+# PostgreSQL's connection details
 conn = psycopg2.connect(
-    dbname="postgres",
-    user="real_priceDB",
-    password="12345678",
-    host="database-1.cf6gq4cgqxg7.ca-central-1.rds.amazonaws.com",
-    port="5432"
+    dbname=os.getenv("DBNAME"),
+    user=os.getenv("USER"),
+    password=os.getenv("PASSWORD"),
+    host=os.getenv("HOST"),
+    port=os.getenv("PORT")
 )
 cur = conn.cursor()
+
+
+# Function to round the timestamp to the nearest half-hour
+def round_to_nearest_half_hour(dt):
+    minutes = dt.minute
+    if minutes < 15:
+        delta = timedelta(minutes=-minutes)
+    elif minutes < 45:
+        delta = timedelta(minutes=30 - minutes)
+    else:
+        delta = timedelta(minutes=60 - minutes)
+    return (dt + delta).replace(second=0, microsecond=0)
+
 
 # Function to get real-time stock price using yfinance
 def get_stock_price(ticker):
     stock = yf.Ticker(ticker)
     todays_data = stock.history(period='1d')
     return todays_data['Close'].iloc[0]
+
 
 # Function to insert stock price data into PostgreSQL
 def insert_stock_price(ticker, price, timestamp):
@@ -26,18 +44,19 @@ def insert_stock_price(ticker, price, timestamp):
     )
     conn.commit()
 
+
 # List of stock tickers
 tickers = ['RDDT', 'GME', 'NVDA', 'RC', 'TSLA', 'AAPL', 'AMZN', 'SPY', 'GOOGL', 'BA', 
            'META', 'ASML', 'MSFT', 'MU', 'INTC', 'NKE', 'HOOD', 'RAMP', 'SPOT', 'QQQ', 
            'VOO', 'AMD', 'PPL', 'ONTO', 'TOON', 'ARM', 'SPY', 'ASML', 'MU', 'EU', 'ICE', 
-           'ES', 'AVGO', 'MSFT', 'HPE', 'SPLG', 'SP', 'GOOG', 'LLY', 'IBM', 'MA', 'NKE', 
+           'ES', 'AVGO', 'MSFT', 'HPE', 'SPLG', 'GOOG', 'LLY', 'IBM', 'MA', 'NKE',
            'CPA', 'WAY', 'DELL', 'SNOW']
 
 # Fetch and store stock prices
 for ticker in tickers:
     try:
         price = get_stock_price(ticker)
-        timestamp = datetime.now()
+        timestamp = round_to_nearest_half_hour(datetime.now())
         insert_stock_price(ticker, price, timestamp)
         print(f"Inserted {ticker}: {price} at {timestamp}")
     except Exception as e:
