7789c7d2134408a9fdb12cb82ac4e1b4c2e090aa
Sam120204 <zhongjiayou1202@gmail.com>
2024-06-27 22:19:07 -0400

    implemented filter_post file and updated fetching posts/comments requirements

--- stock/sentiment_analysis.py
+++
@@ -1,9 +0,0 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-# Initialize VADER sentiment analyzer
-analyzer = SentimentIntensityAnalyzer()
-
-# Function to analyze sentiment
-def analyze_sentiment(text):
-    sentiment_score = analyzer.polarity_scores(text)["compound"]
-    return sentiment_score
